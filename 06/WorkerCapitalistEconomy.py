eda4833f62e5b45bf3ed5e965f66e6d6ccfe3295
Jeppe druedahl <jeppe.druedahl@econ.ku.dk>
2022-02-10 21:40:13 +0100

    General update

--- 06/WorkerCapitalistEconomy.py
+++ 06/WorkerCapitalistEconomy.py
@@ -21,9 +21,11 @@ class WorkerCapitalistEconomyClass():
         par.grid_w = np.linspace(0.1,1.5,par.num_w)
         par.grid_mkt_clearing = np.zeros(par.num_w)
 
-        # c. initial prices
-        self.p = 1 # output price
-        self.w = 1 # wage
+        # c. solution
+        sol = self.sol = SimpleNamespace()
+        
+        sol.p = 1 # output price
+        sol.w = 1 # wage
 
     def utility_w(self,c,l):
         """ utility of workers """
@@ -35,16 +37,18 @@ class WorkerCapitalistEconomyClass():
     def workers(self):
         """ maximize utility for workers """
         
-        p = self.p
-        w = self.w
+        sol = self.sol
+
+        p = sol.p
+        w = sol.w
 
         # a. solve
         obj = lambda l: -self.utility_w((w*l)/p,l) # substitute in the budget constraint
         res = optimize.minimize_scalar(obj,bounds=(0,1),method='bounded')
         
         # b. save
-        self.l_w_star = res.x
-        self.c_w_star = (w*self.l_w_star)/p
+        sol.l_w_star = res.x
+        sol.c_w_star = (w*sol.l_w_star)/p
         
     def utility_c(self,c,l):
         """ utility of capitalists """
@@ -56,24 +60,28 @@ class WorkerCapitalistEconomyClass():
     def capitalists(self):
         """ maximize utility of capitalists """
 
-        p = self.p
-        w = self.w
-        pi = self.pi
+        sol = self.sol
+
+        p = sol.p
+        w = sol.w
+        pi = sol.pi
 
         # a. solve
         obj = lambda l: -self.utility_c((w*l+pi)/p,l) # subsittute in the budget constraint
         res = optimize.minimize_scalar(obj,bounds=(0,1),method='bounded')
         
         # b. save
-        self.l_c_star = res.x
-        self.c_c_star = (w*self.l_c_star+pi)/p
+        sol.l_c_star = res.x
+        sol.c_c_star = (w*sol.l_c_star+pi)/p
         
     def firm(self):
         """ maximize firm profits """
         
-        p = self.p
-        w = self.w
         par = self.par
+        sol = self.sol
+
+        p = sol.p
+        w = sol.w
 
         # a. solve
         f = lambda l: l**par.alpha
@@ -82,37 +90,39 @@ class WorkerCapitalistEconomyClass():
         res = optimize.minimize(obj,x0,bounds=((0,None),),method='L-BFGS-B')
         
         # b. save
-        self.l_star = res.x[0]
-        self.y_star = f(self.l_star)
-        self.Pi = p*self.y_star - w*self.l_star
+        sol.l_star = res.x[0]
+        sol.y_star = f(sol.l_star)
+        sol.Pi = p*sol.y_star-w*sol.l_star
         
     def evaluate_equilibrium(self):
         """ evaluate equilirium """
 
         par = self.par
+        sol = self.sol
 
         # a. optimal behavior of firm
         self.firm()
-        self.pi = self.Pi/par.Nc
+        sol.pi = sol.Pi/par.Nc
 
         # b. optimal behavior of households
         self.workers()
         self.capitalists()
 
         # c. market clearing
-        self.goods_mkt_clearing = par.Nw*self.c_w_star + par.Nc*self.c_c_star - self.y_star
-        self.labor_mkt_clearing = par.Nw*self.l_w_star + par.Nc*self.l_c_star - self.l_star
+        sol.goods_mkt_clearing = par.Nw*sol.c_w_star + par.Nc*sol.c_c_star - sol.y_star
+        sol.labor_mkt_clearing = par.Nw*sol.l_w_star + par.Nc*sol.l_c_star - sol.l_star
     
     def find_equilibrium(self):
 
         par = self.par
+        sol = self.sol
 
         # a. grid search
         print('grid search:')
         for i,w in enumerate(par.grid_w):
-            self.w = w
+            sol.w = w
             self.evaluate_equilibrium()
-            par.grid_mkt_clearing[i] = self.goods_mkt_clearing
+            par.grid_mkt_clearing[i] = sol.goods_mkt_clearing
             print(f' w = {w:.2f} -> {par.grid_mkt_clearing[i]:12.8f}')
         
         print('')
@@ -124,18 +134,18 @@ class WorkerCapitalistEconomyClass():
 
         # c. bisection search
         def obj(w):
-            self.w = w
+            sol.w = w
             self.evaluate_equilibrium()
-            return self.goods_mkt_clearing
+            return sol.goods_mkt_clearing
 
         res = optimize.root_scalar(obj,bracket=[left,right],method='bisect')
-        self.w_eq = res.root
-        print(f'the equilibrium wage is {self.w_eq:.4f}\n')
+        sol.w = res.root
+        print(f'the equilibrium wage is {sol.w:.4f}\n')
 
         # d. show result
-        u_w = self.utility_w(self.c_w_star,self.l_w_star)
-        print(f'workers      : c = {self.c_w_star:6.4f}, l = {self.l_w_star:6.4f}, u = {u_w:7.4f}')
-        u_c = self.utility_c(self.c_c_star,self.l_c_star)
-        print(f'capitalists  : c = {self.c_c_star:6.4f}, l = {self.l_c_star:6.4f}, u = {u_c:7.4f}')        
-        print(f'goods market : {self.goods_mkt_clearing:.8f}')
-        print(f'labor market : {self.labor_mkt_clearing:.8f}')
+        u_w = self.utility_w(sol.c_w_star,sol.l_w_star)
+        print(f'workers      : c = {sol.c_w_star:6.4f}, l = {sol.l_w_star:6.4f}, u = {u_w:7.4f}')
+        u_c = self.utility_c(sol.c_c_star,sol.l_c_star)
+        print(f'capitalists  : c = {sol.c_c_star:6.4f}, l = {sol.l_c_star:6.4f}, u = {u_c:7.4f}')        
+        print(f'goods market : {sol.goods_mkt_clearing:.8f}')
+        print(f'labor market : {sol.labor_mkt_clearing:.8f}')
